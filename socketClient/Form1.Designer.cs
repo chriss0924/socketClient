6b004b40a9d5e827be22ff8ac8ce532f816539ca
unknown <chris.chen@YFY.CORP>
2020-05-27 15:47:21 +0800

    加入專案檔案。

---
+++ socketClient/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace socketClient
+{
+    partial class FormClient
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_ip = new System.Windows.Forms.TextBox();
+            this.textBox_port = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox_ip
+            // 
+            this.textBox_ip.Location = new System.Drawing.Point(141, 13);
+            this.textBox_ip.Name = "textBox_ip";
+            this.textBox_ip.Size = new System.Drawing.Size(166, 22);
+            this.textBox_ip.TabIndex = 0;
+            // 
+            // textBox_port
+            // 
+            this.textBox_port.Location = new System.Drawing.Point(391, 12);
+            this.textBox_port.Name = "textBox_port";
+            this.textBox_port.Size = new System.Drawing.Size(100, 22);
+            this.textBox_port.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(14, 58);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(479, 127);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.TabStop = false;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(14, 208);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(378, 99);
+            this.textBox4.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(108, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "IP：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(339, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "PORT：";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "連線主機";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(398, 208);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 99);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "發送";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // FormClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(505, 326);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox_port);
+            this.Controls.Add(this.textBox_ip);
+            this.Name = "FormClient";
+            this.Text = "Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_ip;
+        private System.Windows.Forms.TextBox textBox_port;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ socketClient/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace socketClient
+{
+    public partial class FormClient : Form
+    {
+        Socket client;
+
+        public FormClient()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AsyncConnect();
+        }
+
+
+        /// <summary>
+        /// 連線到伺服器
+        /// </summary>
+        public void AsyncConnect()
+        {
+            try
+            {
+                //埠及IP
+                IPEndPoint ipe = new IPEndPoint(IPAddress.Parse(textBox_ip.Text), int.Parse(textBox_port.Text));
+                //建立套接字
+                client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                //開始連線到伺服器
+                client.BeginConnect(ipe, asyncResult =>
+                {
+                    client.EndConnect(asyncResult);
+                    //向伺服器傳送訊息
+                    AsyncSend(client, "你好我是客戶端");
+                    //接受訊息
+                    AsyncReceive(client);
+                }, null);
+            }
+            catch (Exception ex)
+            {
+            }
+
+        }
+
+        public void AsyncSend(Socket socket, string message)
+        {
+            if (socket == null || message == string.Empty) return;
+            //編碼
+            byte[] data = Encoding.UTF8.GetBytes(message);
+            try
+            {
+
+                socket.BeginSend(data, 0, data.Length, SocketFlags.None, asyncResult =>
+                {
+                    //完成傳送訊息
+                    int length = socket.EndSend(asyncResult);
+                }, null);
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+
+        public void AsyncReceive(Socket socket)
+        {
+            byte[] data = new byte[1024];
+            try
+            {
+                //開始接收資料
+                socket.BeginReceive(data, 0, data.Length, SocketFlags.None, asyncResult =>
+                    {
+                        try
+                        {
+                            int length = socket.EndReceive(asyncResult);
+                            setText(Encoding.UTF8.GetString(data));
+                        }
+                        catch (Exception)
+                        {
+                            AsyncReceive(socket);
+                        }
+                        AsyncReceive(socket);
+                    }, null);
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            AsyncSend(client, textBox4.Text);
+        }
+
+        private void setText(string str)
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new MethodInvoker(() => setText(str)));
+            }
+            else
+            {
+                textBox3.Text += "\r\n" + str;
+            }
+        }
+
+
+
+    }
+}
